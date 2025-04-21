26456464646a17ffd334a5287e2a7e093fab22f7
Osvaldo3389 <oocaraccioli@gmail.com>
2025-04-21 19:57:00 -0400

    Update banco.h

--- banco.h
+++ banco.h
@@ -1,4 +1,4 @@
-class BancoDePreguntas {
+class BancoDePreguntas { //el Benja es puto 
 private:
     string archivoHistorial;
 
@@ -11,4 +11,20 @@ public:
 
         string linea, preguntaEncontrada;
         int anioEncontrado = -1;
+
+         while(getline(historial, linea)) {
+            if (linea.find("Pregunta: ") == 0) {
+                preguntaEncontrada = linea.substr(10);
+                } else if (linea.find("Año: ") == 0) {
+                anioEncontrado = stoi(linea.substr(5));
+                if (preguntaEncontrada == enunciado && anioEncontrado == anioActual - 1) {
+                    historial.close();
+                    return true;
+                }
+            }
+        }
+        historial.close();
+        return false;
+    }
+
 
