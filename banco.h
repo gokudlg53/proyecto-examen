c3f73809e1eefd82a0517f9660c840828e99b79a
Osvaldo3389 <oocaraccioli@gmail.com>
2025-04-21 19:45:26 -0400

    Update banco.h

--- banco.h
+++ banco.h
@@ -1,1 +1,14 @@
+class BancoDePreguntas {
+private:
+    string archivoHistorial;
+
+public:
+    BancoDePreguntas(string nombreArchivo = "historial_preguntas.txt") : archivoHistorial(nombreArchivo) {}
+
+    bool seUsoEnAnioAnterior(const string& enunciado, int anioActual) {
+        ifstream historial(archivoHistorial);
+        if (!historial.is_open()) return false;
+
+        string linea, preguntaEncontrada;
+        int anioEncontrado = -1;
 
