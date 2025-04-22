3d196ce97c6b2cd6741df1abadc772aa5c8af84f
gokudlg53 <minicarlosganador@gmail.com>
2025-04-21 20:15:15 -0400

    Update prueba.cpp
    agrego librerias y inicio creacion de la clase

--- prueba.cpp
+++ prueba.cpp
@@ -1,4 +1,27 @@
 #ifndef PRUEBA_H
 #define PRUEBA_H
 #include "pregunta.cpp"
- 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+#include <cstdlib>
+using namespace std;
+class Prueba{
+private:
+    int tiempoTotal = 0;
+    Pregunta preguntas[MAX_PREGUNTAS];
+    int cantidadPreguntas = 0;
+    string nombreArchivo;
+    int anioExamen;
+    string asignatura;
+public:
+void limpiarConsola() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
+}
